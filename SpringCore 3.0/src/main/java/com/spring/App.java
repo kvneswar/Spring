b4e01460e0d72069ffe66ce1221f421ccbdc4aee
Eswar <kvn.eswar@hotmail.com>
2015-07-03 11:03:06 -0600

    Spring core 3.0 basic example

---
+++ SpringCore 3.0/src/main/java/com/spring/App.java
@@ -0,0 +1,19 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+    
+    	ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Spring-Module.xml");
+    	Person person = (Person)applicationContext.getBean("person");
+    	System.out.println("First Name: "+person.getFirstName()+", Last Name:"+person.getLastName());
+    	
+    }
+}
---
+++ SpringCore 3.0/src/main/java/com/spring/Person.java
@@ -0,0 +1,24 @@
+package com.spring;
+
+public class Person {
+
+	private String firstName;
+	private String LastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return LastName;
+	}
+	
+	public void setLastName(String lasttName) {
+		LastName = lasttName;
+	}
+	
+}
