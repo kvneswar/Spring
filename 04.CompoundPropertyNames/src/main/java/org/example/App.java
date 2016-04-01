215dcb4a883bdf84b4530a7533ac9bd1344cb24f
Eswar Karumuri <kvn.eswar@hotmail.com>
2016-03-31 20:02:27 -0500

    Simple example for Compound property names.

---
+++ 04.CompoundPropertyNames/src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+	
+    public static void main( String[] args ){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Spring-Config.xml");
+        Person person = applicationContext.getBean("person", Person.class);
+        System.out.println(person);
+    }
+}
