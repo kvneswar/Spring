8d33c252c4c1cd03133f726803948efe52fe31e6
Eswar Karumuri <kvn.eswar@hotmail.com>
2016-03-31 20:42:00 -0500

    Example to use Depends-on element.

---
+++ 05.DependsOn/src/main/java/org/example/DataInitializer.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class DataInitializer {
+	
+	public static Object initializeMeFirst() {
+		System.out.println(DataInitializer.class.getName()+" got initialized");
+		return null;
+	}
+
+}
